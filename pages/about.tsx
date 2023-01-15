3911a4257d339e393bbac21dc8fabe0739760333
gguilt <58526116+gguilt@users.noreply.github.com>
2023-01-15 20:44:12 +0300

    Initialized Next project

---
+++ pages/about.tsx
@@ -0,0 +1,17 @@
+export default function About({ name }) {
+    return (
+        <div>
+            <h1>My name is: {name}</h1>
+        </div>
+    );
+}
+
+export async function getStaticProps() {
+    const data = await fetch("https://randomuser.me/api").then((res) =>
+        res.json()
+    );
+
+    return {
+        props: { name: data.results[0].name.first + " " + data.results[0].name.last },
+    };
+}
