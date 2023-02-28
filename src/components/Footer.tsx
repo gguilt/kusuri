a4048a3fe78b2f2301b4b480f5f5cb80ee6530f2
gguilt <58526116+naphteine@users.noreply.github.com>
2023-02-28 21:15:10 +0300

    Fixed footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,18 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="my-5 pt-5 text-muted text-center text-small">
       <p className="mb-1">All rights reserved. &copy; 2021-2023. Kusuri.</p>
       <ul className="list-inline">
         <li className="list-inline-item">
-          <a href="/privacy">Privacy</a>
+          <Link href="/privacy">Privacy</Link>
         </li>
         <li className="list-inline-item">
-          <a href="/terms">Terms</a>
+          <Link href="/terms">Terms</Link>
         </li>
         <li className="list-inline-item">
-          <a href="/support">Support</a>
+          <Link href="/support">Support</Link>
         </li>
       </ul>
     </footer>
